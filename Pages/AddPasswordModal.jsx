73e50c4f7b2fd1b80c168b6ce22a46608fe86d51
BlackFighter03 <alenat.scarpa@gmail.com>
2024-12-23 11:49:39 +0100

    AddPasswordModal (parte 1)

---
+++ Pages/AddPasswordModal.jsx
@@ -0,0 +1,89 @@
+import { View, Modal, TextInput, Button, Text } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { styles } from '../Components/Graphic features';
+import { useState } from 'react';
+import { FontAwesome } from '@expo/vector-icons';
+import { removeFinalSpaces } from '../Components/removeFinalSpaces';
+
+const AddPasswordModal = ({ websiteTemp, usernameTemp, passwordTemp, visible, onCancel, onSave, setWebsiteTemp, setUsernameTemp, setPasswordTemp }) => {
+
+  /* Costanti che servono a mostrare le informazioni che l'utente non ha messo */
+  const [websiteVoid, setWebsiteVoid] = useState(false);
+  const [usernameVoid, setUsernameVoid] = useState(false);
+  const [passwordVoid, setPasswordVoid] = useState(false);
+  const [showPassword, setShowPassword] = useState(true);
+  const [inputPercentualSetting, setInputPercentualSetting] = useState('50%');
+
+  return (
+    <Modal visible={visible} animationType="slide">
+      <KeyboardAwareScrollView
+        style={styles.container} // Imposta lo stile desiderato per la ScrollView. Assicurati che "flex: 1" sia presente.
+        resetScrollToCoords={{ x: 0, y: 0 }}
+        scrollEnabled={true}
+      >
+        <View style={styles.container}>
+          <View marginTop={inputPercentualSetting}>
+            <Text style={styles.text} marginTop='5%'>Inserisci il nome del sito</Text>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Sito Web"
+              onChangeText={setWebsiteTemp}
+              value={websiteTemp}
+            />
+            <Text style={styles.text} marginTop='5%'>Inserisci l'username (o email) </Text>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Nome Utente"
+              onChangeText={setUsernameTemp}
+              value={usernameTemp}
+            />
+            <Text style={styles.text} marginTop='5%'>Inserisci la password</Text>
+            <View>
+              <TextInput
+                style={styles.pwdInput}
+                placeholder="Password"
+                onChangeText={setPasswordTemp}
+                value={passwordTemp}
+                secureTextEntry={showPassword}
+              />
+              {showPassword ? (
+                <FontAwesome name="eye" style={styles.fontAwesomeEye} onPress={() => setShowPassword(!showPassword)} />
+              ) : (
+                <FontAwesome style={styles.fontAwesomeEye} name="eye-slash" onPress={() => setShowPassword(!showPassword)} />
+              )}
+            </View>
+            {websiteVoid ?
+              <View marginTop="10%">
+                <Text style={styles.textWarning}>Non hai inserito il sito</Text>
+              </View>
+              :
+              null
+            }
+
+            {usernameVoid ?
+              <View marginTop="10%">
+                <Text style={styles.textWarning}>Non hai inserito l'username o l'email</Text>
+              </View>
+              :
+              null
+            }
+
+            {passwordVoid ?
+              <View marginTop="10%">
+                <Text style={styles.textWarning}>Non hai inserito la password</Text>
+              </View>
+              :
+              null
+            }
+            <View style={styles.buttonContainer} marginTop={inputPercentualSetting}>
+              <Button title="Annulla" onPress={reset} color="red" />
+              <Button title="Salva" onPress={handleSave} color="#00e480" />
+            </View>
+          </View>
+        </View>
+      </KeyboardAwareScrollView>
+    </Modal>
+  );
+};
+
+export default AddPasswordModal;
--- Pages/AuthenticatedPage.jsx
+++ Pages/AuthenticatedPage.jsx
@@ -1,373 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import { View, TouchableOpacity, Alert, FlatList, Dimensions } from 'react-native';
-import AddPasswordModal from './AddPasswordModal';
-import Item from '../Components/Item';
-import { styles } from '../Components/Graphic features';
-import * as FileSystem from 'expo-file-system';
-import { ref, getDownloadURL, uploadBytes } from 'firebase/storage';
 import { Header as HeaderRNE, Icon } from '@rneui/themed';
-import { auth, storage } from '../Components/Firebase';
-import { criptaTesto, decriptaTesto } from '../Components/Criptography';
-import sortedStrings from '../Components/PasswordSorting';
-import SideMenu from '../Components/SideMenu';
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
-import ChangePasswordPage from './ChangePasswordPage';
-/**
- * Componente: AuthenticatedScreen
- * Descrizione: Questa schermata viene mostrata dopo l'accesso dell'utente.
- * Gestisce la visualizzazione, l'aggiunta, la modifica e l'eliminazione delle password,
- * salvando i dati sia in Firebase Storage che nel file system del dispositivo.
- */
-const AuthenticatedPage = ({ user, email, password, setPassword, handleAuthentication }) => {
-  // --- Definizione delle costanti ---
+import { View, Text, TextInput, Button, Modal, TouchableOpacity, Alert } from 'react-native';
+import { sendPasswordResetEmail } from '@firebase/auth';
+import { styles } from '../Components/Graphic features';
+import { useState } from 'react';
+import Table from '../Components/Table';
 
-  // Costanti per la gestione del file delle password
-  const fileName = `passwords_${email}.json`;  // Nome del file JSON
-  const pathFirestore = `accounts/${fileName}`; // Percorso del file in Firebase Storage
-  const storageRef = ref(storage, pathFirestore); // Riferimento al file in Firebase Storage
-  const localFilePath = FileSystem.documentDirectory + fileName; // Percorso del file nel file system del dispositivo
+const RecoveryPasswordPage = ({ auth, visible, forgotPasswordEmail, setForgotPasswordEmail, handleSignInPage }) => {
 
-  // --- Definizione dello stato del componente ---
+  const [title, setTitle] = useState("");
+  const [msg, setMsg] = useState("");
+  const [info, setInfo] = useState(false);
 
-  // Array di oggetti che contiene le password dell'utente
-  const [decryptedPasswords, setDecryptedPasswords] = useState([]);
-
-  // Indice dell'elemento correntemente selezionato nell'array 'passwords'
-  const [index, setIndex] = useState(-1);
-
-  // Flag per gestire la visibilità del modale di aggiunta password
-  const [modalVisible, setModalVisible] = useState(false);
-
-  // Flag per gestire la visibilità del modale di modifica password
-  const [modalVisibleForChange, setModalVisibleForChange] = useState(false);
-
-  // Booleana per apertura della tendina
-  const [isOpenSideMenu, setIsOpenSideMenu] = useState(false);
-
-  // Booleana per apertura pagina cambio password
-  const [changePasswordVisible, setChangePasswordVisible] = useState(false);
-
-  // Variabili di stato per i campi di input del modale 
-  const [websiteTemp, setWebsiteTemp] = useState('');
-  const [usernameTemp, setUsernameTemp] = useState('');
-  const [passwordTemp, setPasswordTemp] = useState('');
-
-  // Flag per indicare se è in corso il caricamento del file
-  const [isLoading, setIsLoading] = useState(true); // isLoading è ora gestito come stato
-
-  // --- Effetto collaterale per il download iniziale delle password ---
-
-  useEffect(() => {
-    // Listener per lo stato di autenticazione di Firebase
-    const unsubscribe = auth.onAuthStateChanged(async (user) => {
-      if (user && email) {
-        // Se l'utente è autenticato, scarica le password da Firebase
-        await downloadFile();
-      }
-    });
-
-    // Rimuove il listener quando il componente viene smontato
-    return () => unsubscribe();
-  }, []); // Esegue l'effetto solo quando il componente viene montato o smontato
-
-  // --- Effetto collaterale per aggiornare il file locale quando 'passwords' cambia ---
-
-  useEffect(() => {
-    const updateLocalFile = async () => {
-      if (isLoading) return; // Esci se il file è in caricamento
-      try {
-        const encryptedPasswords = decryptedPasswords.map((item) => ({
-          website: criptaTesto(user.uid, item.website),
-          username: criptaTesto(user.uid, item.username),
-          password: criptaTesto(user.uid, item.password),
-        }));
-
-        await FileSystem.writeAsStringAsync(
-          localFilePath,
-          JSON.stringify(encryptedPasswords)
-        );
-        console.log('File locale aggiornato con successo!');
-        uploadFile();
-      } catch (error) {
-        console.error('Errore durante l\'aggiornamento del file locale:', error);
-      }
-    };
-
-    updateLocalFile(); // Chiama la funzione per aggiornare il file all'avvio
-  }, [decryptedPasswords]); // Esegui l'effetto ogni volta che 'passwords' cambia
-
-  /**
-   * downloadFile: Scarica il file delle password da Firebase Storage.
-   */
-  const downloadFile = async () => {
-    setIsLoading(true); // Inizia il caricamento
+  const handleResetPassword = async () => {
     try {
-      console.log('Provando a scaricare il file...');
-      // Ottiene l'URL di download del file da Firebase Storage
-      const downloadURL = await getDownloadURL(storageRef);
-      console.log('URL di download:', downloadURL);
-      // Scarica il file da Firebase Storage e lo salva nel file system del dispositivo
-      const { uri: downloadedFileUri } = await FileSystem.downloadAsync(downloadURL, localFilePath);
-      console.log('File scaricato in:', downloadedFileUri);
-      // Sposta il file scaricato nella posizione finale nel file system
-      await FileSystem.moveAsync({
-        from: downloadedFileUri,
-        to: localFilePath,
-      });
-      console.log('File spostato con successo!');
-      // Legge il contenuto del file come stringa
-      const fileContent = await FileSystem.readAsStringAsync(localFilePath);
-      console.log('Contenuto del file:\n', fileContent);
-      // Converte il contenuto del file (JSON) in un array di oggetti JavaScript
-      const passwordsData = JSON.parse(fileContent);
-      if (passwordsData.length > 0) {
-        const decryptedData = passwordsData.map((item) => ({
-          website: decriptaTesto(user.uid, item.website),
-          username: decriptaTesto(user.uid, item.username),
-          password: decriptaTesto(user.uid, item.password),
-        }));
-        setDecryptedPasswords(decryptedData);
-      } else {
-        setDecryptedPasswords([]);
-      }
-      setIsLoading(false); // Fine caricamento
+      await sendPasswordResetEmail(auth, forgotPasswordEmail);
+      setTitle("Avviso");
+      setMsg("Email di recupero password inviata!");
+      setInfo(true);
     } catch (error) {
-      // Gestione degli errori durante il download
-      console.log(error);
-      createEmptyFileOnDevice();
-      // Imposta 'passwords' come un array vuoto
-      setDecryptedPasswords([]);
-      setIsLoading(false); // Fine caricamento
+      console.error(error);
+      setTitle("Attenzione");
+      setMsg("Si è verificato un errore durante l'invio dell'email di recupero");
+      setInfo(true);
     }
   };
 
-  /**
-   * createEmptyFileOnDevice: Crea un nuovo file vuoto sul file system del dispositivo.
-   */
-  const createEmptyFileOnDevice = async () => {
-    try {
-      // Scrive un array JSON vuoto nel file specificato
-      await FileSystem.writeAsStringAsync(localFilePath, JSON.stringify([]));
-      console.log('File creato con successo:', localFilePath.uri);
-      // Carica il file vuoto su Firebase Storage
-      uploadFile();
-    } catch (error) {
-      console.error('Errore durante la creazione del file:', error);
-    }
-  };
-
-  /**
-   * uploadFile: Carica il file delle password su Firebase Storage.
-   */
-  const uploadFile = async () => {
-    try {
-      console.log('Inizio caricamento del file...');
-      // Legge il contenuto del file come stringa
-      const fileContent = await FileSystem.readAsStringAsync(localFilePath);
-      console.log('Contenuto del file:\n', fileContent);
-      // Crea un nuovo oggetto Blob dal contenuto del file
-      const blob = new Blob([fileContent], { type: 'application/json' });
-      // Carica il blob su Firebase Storage
-      uploadBytes(storageRef, blob);
-      console.log('File caricato con successo!');
-    } catch (error) {
-      console.error('Errore durante il caricamento del file:', error);
-    }
-  };
-
-  /**
-   * handleAddPassword: Gestisce l'apertura del modale per l'aggiunta di una nuova password.
-   */
-  const handleAddPassword = () => {
-    // Reimposta i campi di input del modale
-    setWebsiteTemp('');
-    setUsernameTemp('');
-    setPasswordTemp('');
-    // Mostra il modale di aggiunta
-    setModalVisible(true);
-  };
-
-    /**
-   * handleSavePassword: Gestisce il salvataggio di una nuova password nell'array e l'upload su Firebase.
-   */
-  const handleSavePassword = async () => {
-    // Chiude il modale
-    setModalVisible(false);
-
-    try {
-      // Crea un nuovo oggetto con i dati della password
-      const newPasswordData = {
-        website: websiteTemp,
-        username: usernameTemp,
-        password: passwordTemp,
-      };
-
-      // Controlla se una password identica esiste già nell'array
-      const isPresented = decryptedPasswords.find(
-        (value) =>
-          value.website === newPasswordData.website &&
-          value.username === newPasswordData.username
-      );
-
-      // Se non ci sono duplicati
-      if (!isPresented) {
-        // Aggiorna lo stato 'passwords' aggiungendo la nuova password e ordinando l'array
-        setDecryptedPasswords((prevPasswords) => [...prevPasswords, newPasswordData].sort(sortedStrings));
-      } else {
-        // Mostra un messaggio di avviso se la password è duplicata
-        Alert.alert('Attenzione', 'Un account con lo stesso sito e nome utente è stato già salvato');
-      }
-
-      // Reimposta i campi di input
-      setWebsiteTemp('');
-      setUsernameTemp('');
-      setPasswordTemp('');
-
-      console.log('Password aggiunta con successo!');
-
-    } catch (error) {
-      console.error("Errore durante l'aggiunta della password:", error);
-    }
-  };
-
-  /**
-   * configureOpenMenu: Configura e apre il menu di gestione della password (modifica/elimina).
-   * Imposta anche lo stato del componente con i dati della password selezionata.
-   */
-  const configureOpenMenu = (item) => {
-    // Trova l'indice dell'elemento selezionato nell'array
-    const index = decryptedPasswords.indexOf(item);
-    // Aggiorna lo stato con l'indice e i dati dell'elemento
-    setIndex(index);
-    setWebsiteTemp(item.website);
-    setUsernameTemp(item.username);
-    setPasswordTemp(item.password);
-  };
-
-  /**
-   * changePassword: Gestisce la modifica di una password esistente.
-   */
-  const changePassword = () => {
-    // Chiude il modale di modifica
-    setModalVisibleForChange(false);
-    // Aggiorna lo stato 'passwords' con la password modificata
-    setDecryptedPasswords((prevPasswords) =>
-      prevPasswords.map((password, i) =>
-        // Modifica solo la password con l'indice corrispondente
-        i === index
-          ? {
-            ...password,
-            website: websiteTemp,
-            username: usernameTemp,
-            password: passwordTemp,
+  return (
+    <Modal visible={visible} animationType="slide">
+      <View style={styles.container}>
+        <HeaderRNE
+          backgroundColor='#00e480'
+          leftComponent={
+            <View>
+              <TouchableOpacity onPress={handleSignInPage}>
+                <Icon type="ionicon" name="return-up-back-outline" color="white" />
+              </TouchableOpacity>
+            </View>
           }
-          : password // Mantiene le altre password invariate
-      ).sort(sortedStrings) // Ordina l'array dopo la modifica
-    );
-
-    // Reimposta lo stato del componente
-    setWebsiteTemp('');
-    setUsernameTemp('');
-    setPasswordTemp('');
-    setIndex(-1);
-  };
-
-  /**
-   * removePassword: Gestisce la rimozione di una password dall'array.
-   */
-  const removePassword = () => {
-    // Aggiorna lo stato 'passwords' rimuovendo la password selezionata
-    setDecryptedPasswords((prevPasswords) =>
-      prevPasswords.filter(
-        (password) =>
-          // Mantiene solo le password che non corrispondono ai dati da rimuovere
-          password.website !== websiteTemp ||
-          password.username !== usernameTemp ||
-          password.password !== passwordTemp
-      )
-    );
-
-    // Reimposta lo stato del componente
-    setWebsiteTemp('');
-    setUsernameTemp('');
-    setPasswordTemp('');
-  };
-
-  
-  return (  
-    <View style={styles.container}>
-      <HeaderRNE
-        backgroundColor="#00e480"
-        leftComponent={
-          <View>
-            <TouchableOpacity onPress={() => setIsOpenSideMenu(true)}>
-               {/**handleAuthentication usata per il logout*/}
-               <MaterialCommunityIcons name="menu" size={24} color="white" />
-            </TouchableOpacity>
+          centerComponent={{ text: 'Recupera password', style: styles.textHeader, onPress: handleSignInPage }}
+        />
+        <View style={styles.container} marginTop='20%'>
+          <Text style={styles.text}>Inserisci la tua email:</Text>
+          <TextInput style={styles.textInput} placeholder="example@email.com" autoCapitalize="none" onChangeText={setForgotPasswordEmail} value={forgotPasswordEmail} />
+          <View style={styles.button} marginTop='15%'>
+            <Button title="Richiedi password" onPress={handleResetPassword} color='#00e480' />
           </View>
-        }
-        centerComponent={{ text: 'Safe Password', style: styles.textHeader }}
-        rightComponent={
-          <View>
-            <TouchableOpacity onPress={handleAddPassword}>
-              <Icon type="materialicons" name="add-circle" color="white" />
-            </TouchableOpacity>
-          </View>
-        }
-      />
-      <SideMenu
-        isOpen={isOpenSideMenu}
-        onClose={() => setIsOpenSideMenu(false)}
-        onLogout={handleAuthentication}
-        setChangePwd={() => setChangePasswordVisible(true)}
-      />
-      <FlatList
-        data={decryptedPasswords}
-        extraData={decryptedPasswords.length}
-        renderItem={({ item }) => (
-          <Item
-            item={item}
-            passwords={decryptedPasswords}
-            configureOpenMenu={configureOpenMenu}
-            setModalVisible={setModalVisibleForChange}
-            removePassword={removePassword}
-          />
-        )}
-      />
-      {/* Modale per aggiungere una nuova password */}
-      <AddPasswordModal
-        visible={modalVisible}
-        usernameTemp={usernameTemp}
-        websiteTemp={websiteTemp}
-        passwordTemp={passwordTemp}
-        onCancel={() => setModalVisible(false)}
-        onSave={handleSavePassword}
-        setWebsiteTemp={setWebsiteTemp}
-        setUsernameTemp={setUsernameTemp}
-        setPasswordTemp={setPasswordTemp}
-      />
-      {/* Modale per modificare una password esistente */}
-      <AddPasswordModal
-        visible={modalVisibleForChange}
-        usernameTemp={usernameTemp}
-        websiteTemp={websiteTemp}
-        passwordTemp={passwordTemp}
-        onCancel={() => setModalVisibleForChange(false)}
-        onSave={changePassword}
-        setWebsiteTemp={setWebsiteTemp}
-        setUsernameTemp={setUsernameTemp}
-        setPasswordTemp={setPasswordTemp}
-      />
-      <ChangePasswordPage
-        auth={auth}
-        email={email}
-        password={password}
-        visible={changePasswordVisible}
-        onClose={() => setChangePasswordVisible(false)}
-        setPassword={setPassword}
-      />
-    </View>
+        </View>
+        <Table
+          visible={info}
+          setVisible={() => {
+            setInfo(false);
+            handleSignInPage();}}
+          title={title}
+          msg={msg}
+        />
+      </View>
+    </Modal>
   );
 };
 
-export default AuthenticatedPage;
+export default RecoveryPasswordPage;
