39092fcaea5edcd01e8b7394c64da6a5cb5a1d63
BlackFighter03 <alenat.scarpa@gmail.com>
2024-12-23 23:11:48 +0100

    Modifica variabile

--- Components/Criptography.jsx
+++ Components/Criptography.jsx
@@ -1,13 +1,13 @@
 const CryptoJS = require("crypto-js");
 
-export const criptaTesto = (email, str) => {
-    const chiaveSegreta = CryptoJS.SHA256(email).toString();
+export const criptaTesto = (key, str) => {
+    const chiaveSegreta = CryptoJS.SHA256(key).toString();
     const testoCifrato = CryptoJS.AES.encrypt(str, chiaveSegreta).toString();
     return testoCifrato;
   };
 
-export const decriptaTesto = (email, str) => {
-    const chiaveSegreta = CryptoJS.SHA256(email).toString();
+export const decriptaTesto = (key, str) => {
+    const chiaveSegreta = CryptoJS.SHA256(key).toString();
     const bytes  = CryptoJS.AES.decrypt(str, chiaveSegreta);
     const testoDecodificato = bytes.toString(CryptoJS.enc.Utf8);
     return testoDecodificato;
